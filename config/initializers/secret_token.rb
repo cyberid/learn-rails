f7092211f649e4d1884da59747561805b352007e
cyberid <jeni@jeni-System.(none)>
2014-03-20 10:57:33 +0700

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnRails::Application.config.secret_key_base = 'ad57dc01f2a18e4714b150f236f60da007e4b7f15a1686a4141ce9e66ff0d1d5a8f59a8503b3751bb1b5dc062c337a35df998e88ba010e48c1636cc14e5fe09f'
