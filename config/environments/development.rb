03b35e9a86ae69ffaaf554231d7e230e5a95c69c
cyberid <jeni@jeni-System.(none)>
2014-03-21 09:32:43 +0700

    edit konfig

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,7 @@ LearnRails::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  
 
   config.action_mailer.smtp_settings = {
 address: "smtp.gmail.com",
@@ -39,5 +40,6 @@ password: ENV["GMAIL_PASSWORD"]
 
 # Send email in development mode.
 config.action_mailer.perform_deliveries = true
+
 
 end
